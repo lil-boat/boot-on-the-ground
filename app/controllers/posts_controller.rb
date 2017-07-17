be1df5d8bbc3ea5a418bd482c5f08d8f482b4480
iand11 <ian.driscoll.o@gmail.com>
2017-07-17 15:33:46 -0700

    Add working routes for everything

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def show
+    
+  end
+
+  def destroy
+    
+  end
+end
---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,23 @@
+class ProjectsController < ApplicationController
+
+  def index
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def destroy
+    
+  end
+
+end
