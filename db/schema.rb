a46fe40107b07d815d4de4add2d889407c614e89
Natalie Yip <yip.natalie92@gmail.com>
2017-07-17 16:18:59 -0700

    Add Company Index

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20170717193128) do
     t.string "body", null: false
     t.integer "post_id"
     t.integer "user_id"
-    t.integer "likes_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
