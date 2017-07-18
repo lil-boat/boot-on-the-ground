35a4132ffebbe5483d8faef16dd42dab057904d4
Brian Wong <brianwong92@gmail.com>
2017-07-17 17:03:08 -0700

    add username, testimonial, and title to profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,23 @@
 class UsersController < ApplicationController
-  
+
   def new
-    
+
   end
 
   def create
-    
+
   end
 
   def edit
-    
+
   end
 
   def show
-    
+    @user = User.find(params[:id])
+    @job = Job.where(user_id: params[:id])
   end
 
   def destroy
-    
+
   end
 end
