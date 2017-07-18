20848d03b761e8b2734776e6bfe8d30e54c4d1de
Natalie Yip <yip.natalie92@gmail.com>
2017-07-17 17:45:00 -0700

    Add bare bones structure for edit and delete routes

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -10,15 +10,26 @@ class CompaniesController < ApplicationController
 
   def create
     @company = Company.create(company_params)
-    redirect_to '/companies'
+    redirect_to companies_path
   end
 
   def edit
-    
+    @company = Company.find(params[:id])
+  end
+
+  def update
+    @company = Company.find(params[:id])
+     if @company.update(params[:company].permit(:name, :description, :logo, :url ))
+      redirect_to companies_path
+    else
+      render 'edit'
+    end 
   end
 
   def destroy
-    
+    @company = Company.find(params[:id])
+    @company.destroy
+    redirect_to company_path
   end
 
   private 
