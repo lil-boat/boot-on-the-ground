559024cf5042bc9e544ac26b4473d864df58eb54
Merty McGraw <mertymcgraw@gmail.com>
2017-07-18 08:51:47 -0700

    Add errors partial working on create new post. Need to figure out how to assign the user upon creation

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :new, :show, :create, :edit]
   resources :sessions, only: [:new]
   resources :projects, only: [:new, :index, :edit, :destroy]
-  resources :posts, only: [:new, :index, :edit, :show, :destroy]
+  resources :posts, only: [:new, :create, :index, :edit, :show, :destroy]
   resources :companies, only: [:new, :index, :edit, :destroy]
   resources :jobs, only: [:index, :new, :edit, :destroy]
 end
