b849b41f78bc35a2ae23aec891a32acf47db0f05
Merty McGraw <mertymcgraw@gmail.com>
2017-07-19 16:30:56 -0700

    fix current user error for liking posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,11 @@
 class LikesController < ApplicationController
   def create
-    @like = Like.new(likeable_id: likes_params, likeable_type: "Post", user_id: 1)
+    @like = Like.new(likeable_id: likes_params, likeable_type: "Post", user_id: current_user.id)
     if @like.save
       like_count = Post.find(likes_params).likes.count
       render json: {like_count: like_count}.to_json
     else 
-      "yikes"
+      "yikes, something went wrong"
     end
    
   end
