f71b2a0760a6f8266995e0424e6d609fe8f33e08
Natalie Yip <yip.natalie92@gmail.com>
2017-07-19 16:41:00 -0700

    Add paperclip capabilities to projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -45,6 +45,6 @@ class ProjectsController < ApplicationController
 
 private
     def project_params
-      params.require(:project).permit(:title, :description)
+      params.require(:project).permit(:title, :description, :image)
     end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,6 @@ class Project < ApplicationRecord
   has_many :users, through: :user_projects
   has_many :likes, as: :likeable
 
-  # has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
-  # validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
