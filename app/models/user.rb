b9501b8054a38dd183ba1ec125035645018157cd
Natalie Yip <yip.natalie92@gmail.com>
2017-07-19 17:03:16 -0700

    Make paperclip work for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,7 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
 end
