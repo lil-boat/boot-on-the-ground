ad5ca006754119f7eec32295c6c6053e5a0e3a80
Brian Wong <brianwong92@gmail.com>
2017-07-21 11:52:49 -0700

    add closeLogin logic

---
+++ app/assets/javascripts/signup.js
@@ -0,0 +1,9 @@
+function openSignUp() {
+  document.getElementById('signup-popup').style.width = '100%';
+}
+
+function closeSignUp() {
+  document.getElementById('signup-popup').style.width = '0';
+  document.body.style.backgroundColor = 'white';
+  closeLogin();
+}
